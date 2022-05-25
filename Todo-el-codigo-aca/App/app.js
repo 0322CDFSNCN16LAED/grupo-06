3bde4bec34caac24ace92b790b523da0646a617d
Manufuentemoreno <manu-u@live.com.ar>
2022-05-24 21:23:31 -0300

    unifiqué Home y Product Detail en App

--- Todo-el-codigo-aca/App/app.js
+++ Todo-el-codigo-aca/App/app.js
@@ -8,7 +8,11 @@ app.use(express.static(publicPath))  //usamos la funcion static de express, que 
 
 app.listen(3000, () => console.log("Servidor ejecutándose en el puerto 3000"));
 
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
 app.get("/product-name",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
 })
-
